b18ab5421e24bb92b3cfca054b5127f8c1a317d1
Troy Chin <ttroychin@gmail.com>
2025-02-15 17:35:05 -0800

    Preparing for implementation of Warnsdorff's Rule

---
+++ Python-Assignments/warnsdorffs_rule.py
@@ -0,0 +1,8 @@
+"""
+File name: warnsdorffs_rule.py
+Author: Troy Chin, Jacob Hellebrand, Anderson Pham, Cristian Verduzco, Christian Barajas
+Date: 2025 Feb 15
+Description: This script implements Warnsdorff's rule for solving the knight's tour problem.
+Version: 1.0
+"""
+
